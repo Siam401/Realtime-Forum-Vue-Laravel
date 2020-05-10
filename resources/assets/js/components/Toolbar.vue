328a81419f719a9cd3dbf97449f1a681dc76cc1b
Siam <armansiam22@gmail.com>
2020-05-10 10:37:44 +0600

    Vuetify and Vue-router install

---
+++ resources/assets/js/components/Toolbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card
+    color="grey lighten-4"
+    flat
+    height="200px"
+    tile
+  >
+    <v-toolbar dense>
+
+      <v-toolbar-title>QNA Forum</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn flat>Forum</v-btn>
+      <v-btn flat>Ask Question</v-btn>
+      <v-btn flat>Category</v-btn>
+      <v-btn flat>Login</v-btn>
+    </v-toolbar>
+  </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
